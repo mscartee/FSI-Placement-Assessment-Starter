414110565f70fa0241e440fd75bb3d5f01242509
Michael Cartee <admin@asgardcrolingai.lan>
2022-02-01 18:17:20 -0500

    midway through starting

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Michael Cartee" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
+function updateQuantity-gb(displayQuantity){
+    let quantity=document.querySelector('.qty-gb')
+    quantity.innerHTML=displayQuantity
+}
+
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
 // selecting the element with an id of add-gb
 const gbPlusBtn = document.querySelector('#add-gb')
-
+const gbMinusBtn = document.querySelector('#minus-gb')
+/ 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
